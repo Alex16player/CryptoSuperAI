719116b4c16aa997934bb86ea591b093fae64c7e
Alex16player <alexjunker3@gmail.com>
2025-04-18 18:41:31 +0200

    ensemble_model.py

--- super_ai/ensemble_model.py
+++ super_ai/ensemble_model.py
@@ -1,11 +1,8 @@
 # super_ai/ensemble_model.py
 
-import joblib
+class DummyModel:
+    def predict(self, X):
+        return [1]  # Dummy: gibt immer "buy" zurück
 
-# Dummy-Loader für Modell (kann später durch echtes Modell ersetzt werden)
 def load_ensemble_model(path=None):
-    # Du kannst später hier ein echtes Modell mit joblib.load(path) laden
-    class DummyModel:
-        def predict(self, X):
-            return [1]  # 1 = BUY, 0 = SELL
     return DummyModel()
